a35577fa84f18f90b74befb5ae0a0181b05a7d4c
Trevor Winn <35859863+ClumsyBuddy@users.noreply.github.com>
2021-11-14 13:38:14 -0500

    Small visionmanager changes

--- VisionManager.py
+++ VisionManager.py
@@ -9,7 +9,6 @@ import numpy
 class ShotType(enum.Enum):
     Single = 1
     Multiple = 2
-    Section = 3
 
 
 class Handle_Images:
@@ -54,9 +53,6 @@ class Handle_Images:
         elif Type == self._Type.Multiple:
             print("Multiple is not working Currently")
             file_Name = self.screen_shot.SingleScreenShot(x=x,y=y,w=w,h=h)
-        elif Type == self._Type.Section:
-            file_Name = self.screen_shot.ScreenSection(x=x,y=y,w=w,h=h)
-        #Curr_Img = cv.imread(file_Name) Switched to holding picture in memory
         self.Curr_img_grey = cv.cvtColor(file_Name, cv.COLOR_RGB2GRAY)
 
     def Template(self):
@@ -99,9 +95,10 @@ class ScreenShot:
     _y = 0
     _w = 0
     _h = 0
-    def SingleScreenShot(Directory=_Directory, x=_x, y=_y, w=_w, h=_h):
+    def SingleScreenShot(Directory=_Directory, x = 0, y = 0, w = auto.size().width, h = auto.size().height):
+        monitor = {"top": y, "left": x, "width": w, "height": h}
         with mss.mss() as sct:
-            filename = numpy.array(sct.grab({"mon":1,"top":y, "left":x, "width":w, "height":h}))
+            filename = numpy.array(sct.grab(monitor))
         return filename
 
     def MultipleScreenShots(Directory=_Directory, x=_x, y=_y, w=_w, h=_h):
@@ -111,13 +108,3 @@ class ScreenShot:
                 file_names.append(sct.shot(mon=-1, output=Directory+str(i)))
         return file_names
 
-    def ScreenSection(Directory=_Directory, x=_x, y=_y, w=_w, h=_h):
-        with mss.mss() as sct:
-            # The screen part to capture
-            monitor = {"top": y, "left": x, "width": w, "height": h}
-            filename = Directory.format(**monitor)
-            # Grab the data
-            sct_img = sct.grab(monitor)
-            # Save to the picture file
-            mss.tools.to_png(sct_img.rgb, sct_img.size, output=filename)
-        return filename
